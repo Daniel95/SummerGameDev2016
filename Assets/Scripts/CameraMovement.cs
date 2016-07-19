99e32796787353b5e00515eb3003e4a5821ad9c9
Patrick-Aubroeck <P.aubroeck@hotmail.com>
2016-07-19 12:30:38 +0200

    Cam touch movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour
+{
+    private float speed = 2.0f;
+    private float zoomSpeed = 2.0f;
+
+    public float maxYLoc = 14.0f;
+    public float minZLoc = -14.0f;
+
+
+    public float minY = -25.0f;
+    public float maxY = 75.0f;
+
+    public float sensX = 100.0f;
+    public float sensY = 7.3f;
+
+    private float rotYMultiPlyer = 100.0f;
+
+    float rotationY = 0.0f;
+
+    public void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (transform.rotation.x >= maxY || transform.rotation.x <= minY)
+            return;
+        transform.Translate(0, scroll * zoomSpeed, scroll * zoomSpeed, Space.World);
+        rotationY += scroll * (sensY * rotYMultiPlyer) * Time.deltaTime;
+        rotationY = Mathf.Clamp(rotationY, minY, maxY);
+        transform.localEulerAngles = new Vector3(rotationY, 0, 0);
+    }
+}
---
+++ Assets/Scripts/TouchCameraControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchCameraControl : MonoBehaviour
+{
+    private float minFieldOfView = 0.1f;
+    private float maxFieldOfView = 179.9f;
+    public float perspectiveZoomSpeed = 0.5f;
+
+    [SerializeField]
+    private Camera _camera;
+
+    void Update()
+    {
+        // If there are two touches on the device...
+        if (Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // Otherwise change the field of view based on the change in distance between the touches.
+            _camera.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+            // Clamp the field of view to make sure it's between 0 and 180.
+            _camera.fieldOfView = Mathf.Clamp(_camera.fieldOfView, minFieldOfView, maxFieldOfView);
+        }
+    }
+}
