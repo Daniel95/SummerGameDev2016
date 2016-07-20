093869eb5afd5969a31447624620ad8eb9ed6103
Patrick-Aubroeck <P.aubroeck@hotmail.com>
2016-07-20 11:06:46 +0200

    Start For interaction

---
+++ Assets/Scripts/Obstacles/PointsBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PointsBehaviour : MonoBehaviour, IObstaclesBehaviour {
+
+    public GameObject ballObject;
+
+
+    public void Interact()
+    {
+      
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    public void OnTriggerEnter(Collider collider)
+    {
+
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+
+    }
+}
