ed1c294d53f6d81d037e8634eb5271e8582025e7
Patrick-Aubroeck <P.aubroeck@hotmail.com>
2016-07-20 14:07:25 +0200

    Name Changes, File location Changes

---
+++ Assets/Scripts/Controllers/ScoreTracker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour {
+
+    [SerializeField]
+    private Text Text;
+
+    private int shotCount = 0;
+
+    public void UnitedCoinCollected()
+    {
+        Text.text = "POINTS";
+    }
+
+    public void IncreaseShotCount()
+    {
+        shotCount++;
+        Text.text = shotCount.ToString();
+    }
+}
--- Assets/Scripts/Obstacles/UnitedCoinTrigger.cs
+++ Assets/Scripts/Obstacles/UnitedCoinTrigger.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
 
-public class PointsBehaviour : MonoBehaviour, IObstaclesBehaviour {
+public class UnitedCoinTrigger : MonoBehaviour, IObstaclesBehaviour {
 
-    public GameObject ballObject;
-    public GameObject scoreCalc;
+    private ScoreTracker scoreCalc;
 
     public void Interact()
     {
+        scoreCalc.UnitedCoinCollected();
         Debug.Log("Hit Test");
         //Destroy(this);
     }
 
     // Use this for initialization
     void Start () {
-        scoreCalc = GameObject.FindGameObjectWithTag(Tags.GAMECONTROLLER.ToString());
+        scoreCalc = GameObject.FindGameObjectWithTag(Tags.GAMECONTROLLER.ToString()).GetComponent<ScoreTracker>();
 	}
 
     private void OnTriggerEnter(Collider collider)
