9f001515b8d0870c267f992b503e2933be787173
Patrick-Aubroeck <P.aubroeck@hotmail.com>
2016-07-21 19:57:31 +0200

    Scenes, audio

---
+++ Assets/Scripts/Controllers/Audio.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Audio : MonoBehaviour {
+
+    [SerializeField]
+    private Image audioImage;
+
+    [SerializeField]
+    private Sprite audioOn;
+
+    [SerializeField]
+    private Sprite audioOff;
+
+    private bool isAudioOn;
+
+    private void Start()
+    {
+        isAudioOn = true;
+    }
+
+    public void SetAudioImage()
+    {
+        if (isAudioOn)
+        {
+            audioImage.sprite = audioOff;
+            isAudioOn = false;
+        }
+        else
+        {
+            audioImage.sprite = audioOn;
+            isAudioOn = true;
+        }
+    }
+
+
+}
